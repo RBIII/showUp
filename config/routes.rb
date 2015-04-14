e6478981758ff50d671ff5ae9ece65d7776abe56
Rocco Basso <basso.r@husky.neu.edu>
2015-04-14 16:36:14 -0400

    removed unnessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'shows#index'
   devise_for :users
 
-  resources :venues, only: [:index, :show, :create] do
+  resources :venues, only: [:index, :show] do
     resources :reviews, only: [:create]
   end
 
