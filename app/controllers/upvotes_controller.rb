f6534b2ec8e76543d82c3e45f6d8992d280ff5dd
Rocco <basso.r@husky.neu.edu>
2015-07-20 13:48:14 -0400

    refactored voting javascript

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -3,14 +3,14 @@ class UpvotesController < ApplicationController
 
   def create
     @show = Show.find(params[:show_id])
-    if Vote.find_by(show: @show)
-      @last_value = Vote.find_by(show: @show).value
+    unless Vote.where("user_id = ? and show_id = ?", current_user.id, params[:show_id]).empty?
+      @last_value = Vote.where("user_id = ? and show_id = ?", current_user.id, params[:show_id]).first.value
     else
       @last_value = 0
     end
 
     VoteManager.new(current_user, @show).upvote
-    @vote = Vote.find_by(show: @show)
+    @vote = Vote.where("user_id = ? and show_id = ?", current_user.id, params[:show_id]).first
     respond_to do |format|
       format.html {redirect_to @show}
       format.js
